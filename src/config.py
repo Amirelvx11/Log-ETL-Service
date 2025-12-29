226d75cb43f905d4ae3323f3fbe5879bf49b95c5
Amirelvx11 <jamshidiamirmahdi1@gmail.com>
2025-12-29 17:02:01 +0330

    remove src/constants and add it into the src/config

--- src/config.py
+++ src/config.py
@@ -28,3 +28,19 @@ mssql_engine = create_engine(
     pool_recycle=1800,
     connect_args={"timeout": 5, "LoginTimeout": 5},
 )
+
+PART_ID_BY_PREFIX = {
+    "AF70": "A3925DD2-F7C3-4E27-B487-E547F8F980E2",
+    "AF75": "B159B8DA-AD61-4C25-97C8-C82CF7955D06",
+}
+
+COMM_MODE_MAP = {
+    0: 4,
+    1: 1,
+    2: 2,
+}
+
+REQUEST_TYPE_MAP = {
+    "RUA": 0,
+    "RAL": 1,
+}
