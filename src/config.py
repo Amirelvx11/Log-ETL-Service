a3a673366ce559db4010b534a6b7380123b9f5f2
Amirelvx11 <jamshidiamirmahdi1@gmail.com>
2025-12-29 14:30:09 +0330

    add config files based on three core source db (my sql) target db (ms sql) and user guid in crm (like TMS-ETL-Service project)

---
+++ src/config.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+load_dotenv()
+
+def _require_env(key: str) -> str:
+    """Ensure required environment variables are present."""
+    value = os.getenv(key)
+    if not value:
+        raise RuntimeError(f"Missing required env var: {key}")
+    return value
+
+# Required ENV variables
+SOURCE_DB = _require_env("SOURCE_DB") # pyodbc SQLAlchemy URI
+TARGET_DB = _require_env("TARGET_DB") # pyodbc SQLAlchemy URI
+USER_GUID = _require_env("USER_GUID") # MUST be valid in system.User.Id
+
+# Engines (MySQL and MSSQL)
+src_engine = create_engine(SOURCE_DB, pool_pre_ping=True)
+dst_engine = create_engine(
+    TARGET_DB,
+    pool_pre_ping=True,
+    fast_executemany=True,
+    max_overflow=5,
+    pool_timeout=5,
+    pool_recycle=1800,
+    connect_args={"timeout": 5, "LoginTimeout": 5},
+)
