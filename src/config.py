10577742b5af5e82cce216522e1c882bf6b36980
Amirelvx11 <jamshidiamirmahdi1@gmail.com>
2025-12-30 14:38:37 +0330

    change src/config , add pool size method in ms sql engine

--- src/config.py
+++ src/config.py
@@ -5,7 +5,6 @@ from sqlalchemy import create_engine
 load_dotenv()
 
 def _require_env(key: str) -> str:
-    """Ensure required environment variables are present."""
     value = os.getenv(key)
     if not value:
         raise RuntimeError(f"Missing required env var: {key}")
@@ -15,7 +14,9 @@ def _require_env(key: str) -> str:
 SOURCE_DB = _require_env("SOURCE_DB")   # SQLAlchemy URI (MySQL)
 TARGET_DB = _require_env("TARGET_DB")   # SQLAlchemy URI (MSSQL)
 USER_GUID = _require_env("USER_GUID")   # MUST exist in system.User.Id
-BATCH_SIZE = int(os.getenv("BATCH_SIZE", "2000")) #ensure no limitation based on mssql insert
+
+# OPTIONAL
+BATCH_SIZE = int(os.getenv("BATCH_SIZE", "10000")) #ensure that no limitation based on mssql and your RAM for insert
 
 # Engines (MySQL-source and MSSQL-target)
 mysql_engine = create_engine(SOURCE_DB, pool_pre_ping=True)
@@ -23,11 +24,13 @@ mssql_engine = create_engine(
     TARGET_DB,
     pool_pre_ping=True,
     fast_executemany=True,
+    pool_size=5,
     max_overflow=5,
     pool_timeout=5,
     pool_recycle=1800,
     connect_args={"timeout": 5, "LoginTimeout": 5},
 )
+
 
 PART_ID_BY_PREFIX = {
     "AF70": "A3925DD2-F7C3-4E27-B487-E547F8F980E2",
