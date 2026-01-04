0f8498ebea01a3b0fca9cbd2c0740d0e9c213727
Amirelvx11 <jamshidiamirmahdi1@gmail.com>
2026-01-04 10:08:00 +0330

    change main.py, use proper and correct logging method and batch processing with complete and robust log system in all levels for production (info, error, warning & etc). also use complete error & exceptions info stages in logger.

--- main.py
+++ main.py
@@ -9,43 +9,58 @@ logger = get_logger(__name__)
 
 def run_etl():
     with RunTracker("device-log-etl") as run:
+        current_run_id = run.run_id 
+        
         try:
-            last_id = get_last_inserted_tms_id()
-            logger.info("log-etl started", extra={"last_tms_id": last_id})
+            last_id = get_last_inserted_tms_id(current_run_id)
+            logger.info(
+                "log-etl started",
+                extra={"last_tms_log_id": last_id, "run_id": current_run_id},
+            )
 
+            batch_no = 0
             total_inserted = 0
 
             while True:
-                df = fetch_source_rows(last_id)
+                df = fetch_source_rows(last_id, current_run_id)
                 if df.empty:
                     break
 
-                transformed = transform_rows(df, USER_GUID)
-                inserted = insert_rows(transformed)
-
-                total_inserted += inserted
-                last_id = int(df["id"].max())
+                batch_no += 1
+                max_id = int(df["id"].max())
 
                 logger.info(
-                    "batch completed",
+                    "processing batch",
                     extra={
-                        "after_tms_id": last_id,
-                        "batch_inserted": inserted,
+                        "batch_no": batch_no,
+                        "from_tms_log_id": last_id + 1,
+                        "to_tms_log_id": max_id,
+                        "row_count": len(df),
+                        "run_id": current_run_id,
                     },
                 )
+
+                transformed = transform_rows(df, USER_GUID)
+                inserted = insert_rows(transformed, current_run_id)
+
+                total_inserted += inserted
+                last_id = max_id
 
             logger.info(
                 "log-etl finished",
                 extra={
-                    "final_tms_id": last_id,
-                    "total_inserted": total_inserted,
+                    "final_last_tms_log_id": last_id,
+                    "batches": batch_no,
+                    "rows_inserted": total_inserted,
+                    "run_id": current_run_id
                 },
             )
-
+            
         except Exception as exc:
             logger.error(
                 "log-etl crashed",
-                extra={"error": str(exc), "run_id": run.run_id},
+                extra={"error": str(exc), "run_id": current_run_id},
+                exc_info=True,
             )
             raise
       
